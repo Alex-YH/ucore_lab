5e9ac405f765afc62eb3749ebcde1c2a6b8741c5
Alex-YH <hyangthu@163.com>
2015-05-07 17:49:08 +0800

    finished lab6

--- labcodes/lab6/kern/schedule/sched.c
+++ labcodes/lab6/kern/schedule/sched.c
@@ -1,176 +1,72 @@
+#ifndef __KERN_SCHEDULE_SCHED_H__
+#define __KERN_SCHEDULE_SCHED_H__
+
+#include <defs.h>
 #include <list.h>
-#include <sync.h>
-#include <proc.h>
-#include <sched.h>
-#include <stdio.h>
-#include <assert.h>
-#include <default_sched.h>
+#include <skew_heap.h>
 
-// the list of timer
-static list_entry_t timer_list;
+#define MAX_TIME_SLICE 5
 
-static struct sched_class *sched_class;
+struct proc_struct;
 
-static struct run_queue *rq;
+typedef struct {
+    unsigned int expires;       //the expire time
+    struct proc_struct *proc;   //the proc wait in this timer. If the expire time is end, then this proc will be scheduled
+    list_entry_t timer_link;    //the timer list
+} timer_t;
 
-static inline void
-sched_class_enqueue(struct proc_struct *proc) {
-    if (proc != idleproc) {
-        sched_class->enqueue(rq, proc);
-    }
+#define le2timer(le, member)            \
+to_struct((le), timer_t, member)
+
+// init a timer
+static inline timer_t *
+timer_init(timer_t *timer, struct proc_struct *proc, int expires) {
+    timer->expires = expires;
+    timer->proc = proc;
+    list_init(&(timer->timer_link));
+    return timer;
 }
 
-static inline void
-sched_class_dequeue(struct proc_struct *proc) {
-    sched_class->dequeue(rq, proc);
-}
+struct run_queue;
 
-static inline struct proc_struct *
-sched_class_pick_next(void) {
-    return sched_class->pick_next(rq);
-}
+// The introduction of scheduling classes is borrrowed from Linux, and makes the 
+// core scheduler quite extensible. These classes (the scheduler modules) encapsulate 
+// the scheduling policies. 
+struct sched_class {
+    // the name of sched_class
+    const char *name;
+    // Init the run queue
+    void (*init)(struct run_queue *rq);
+    // put the proc into runqueue, and this function must be called with rq_lock
+    void (*enqueue)(struct run_queue *rq, struct proc_struct *proc);
+    // get the proc out runqueue, and this function must be called with rq_lock
+    void (*dequeue)(struct run_queue *rq, struct proc_struct *proc);
+    // choose the next runnable task
+    struct proc_struct *(*pick_next)(struct run_queue *rq);
+    // dealer of the time-tick
+    void (*proc_tick)(struct run_queue *rq, struct proc_struct *proc);
+    /* for SMP support in the future
+     *  load_balance
+     *     void (*load_balance)(struct rq* rq);
+     *  get some proc from this rq, used in load_balance,
+     *  return value is the num of gotten proc
+     *  int (*get_proc)(struct rq* rq, struct proc* procs_moved[]);
+     */
+};
 
-static void
-sched_class_proc_tick(struct proc_struct *proc) {
-    if (proc != idleproc) {
-        sched_class->proc_tick(rq, proc);
-    }
-    else {
-        proc->need_resched = 1;
-    }
-}
+struct run_queue {
+    list_entry_t run_list;
+    unsigned int proc_num;
+    int max_time_slice;
+    // For LAB6 ONLY
+    skew_heap_entry_t *lab6_run_pool;
+};
 
-static struct run_queue __rq;
+void sched_init(void);
+void wakeup_proc(struct proc_struct *proc);
+void schedule(void);
+void add_timer(timer_t *timer);     // add timer to timer_list
+void del_timer(timer_t *timer);     // del timer from timer_list
+void run_timer_list(void);          // call scheduler to update tick related info, and check the timer is expired? If expired, then wakup proc
 
-void
-sched_init(void) {
-    list_init(&timer_list);
-
-    sched_class = &default_sched_class;
-
-    rq = &__rq;
-    rq->max_time_slice = MAX_TIME_SLICE;
-    sched_class->init(rq);
-
-    cprintf("sched class: %s\n", sched_class->name);
-}
-
-void
-wakeup_proc(struct proc_struct *proc) {
-    assert(proc->state != PROC_ZOMBIE);
-    bool intr_flag;
-    local_intr_save(intr_flag);
-    {
-        if (proc->state != PROC_RUNNABLE) {
-            proc->state = PROC_RUNNABLE;
-            proc->wait_state = 0;
-            if (proc != current) {
-                sched_class_enqueue(proc);
-            }
-        }
-        else {
-            warn("wakeup runnable process.\n");
-        }
-    }
-    local_intr_restore(intr_flag);
-}
-
-void
-schedule(void) {
-    bool intr_flag;
-    struct proc_struct *next;
-    local_intr_save(intr_flag);
-    {
-        current->need_resched = 0;
-        if (current->state == PROC_RUNNABLE) {
-            sched_class_enqueue(current);
-        }
-        if ((next = sched_class_pick_next()) != NULL) {
-            sched_class_dequeue(next);
-        }
-        if (next == NULL) {
-            next = idleproc;
-        }
-        next->runs ++;
-        if (next != current) {
-            proc_run(next);
-        }
-    }
-    local_intr_restore(intr_flag);
-}
-
-// add timer to timer_list
-void
-add_timer(timer_t *timer) {
-    bool intr_flag;
-    local_intr_save(intr_flag);
-    {
-        assert(timer->expires > 0 && timer->proc != NULL);
-        assert(list_empty(&(timer->timer_link)));
-        list_entry_t *le = list_next(&timer_list);
-        while (le != &timer_list) {
-            timer_t *next = le2timer(le, timer_link);
-            if (timer->expires < next->expires) {
-                next->expires -= timer->expires;
-                break;
-            }
-            timer->expires -= next->expires;
-            le = list_next(le);
-        }
-        list_add_before(le, &(timer->timer_link));
-    }
-    local_intr_restore(intr_flag);
-}
-
-// del timer from timer_list
-void
-del_timer(timer_t *timer) {
-    bool intr_flag;
-    local_intr_save(intr_flag);
-    {
-        if (!list_empty(&(timer->timer_link))) {
-            if (timer->expires != 0) {
-                list_entry_t *le = list_next(&(timer->timer_link));
-                if (le != &timer_list) {
-                    timer_t *next = le2timer(le, timer_link);
-                    next->expires += timer->expires;
-                }
-            }
-            list_del_init(&(timer->timer_link));
-        }
-    }
-    local_intr_restore(intr_flag);
-}
-
-// call scheduler to update tick related info, and check the timer is expired? If expired, then wakup proc
-void
-run_timer_list(void) {
-    bool intr_flag;
-    local_intr_save(intr_flag);
-    {
-        list_entry_t *le = list_next(&timer_list);
-        if (le != &timer_list) {
-            timer_t *timer = le2timer(le, timer_link);
-            assert(timer->expires != 0);
-            timer->expires --;
-            while (timer->expires == 0) {
-                le = list_next(le);
-                struct proc_struct *proc = timer->proc;
-                if (proc->wait_state != 0) {
-                    assert(proc->wait_state & WT_INTERRUPTED);
-                }
-                else {
-                    warn("process %d's wait_state == 0.\n", proc->pid);
-                }
-                wakeup_proc(proc);
-                del_timer(timer);
-                if (le == &timer_list) {
-                    break;
-                }
-                timer = le2timer(le, timer_link);
-            }
-        }
-        sched_class_proc_tick(current);
-    }
-    local_intr_restore(intr_flag);
-}
+#endif /* !__KERN_SCHEDULE_SCHED_H__ */
